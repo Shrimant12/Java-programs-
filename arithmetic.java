1048ec1b10dd23140321caa2c6605c3484321272
Shrimant More <smshrimant@gmail.com>
2018-08-07 01:35:39 +0530

    New file added

---
+++ arithmetic.java
@@ -0,0 +1,46 @@
+//Simple arithmetic operations 
+
+import java.util.Scanner;
+
+class arithmetic
+{
+	
+	public static void main(String args[])
+	{
+				
+			int x,y;
+			
+			Scanner sc = new Scanner(System.in);
+
+
+			
+			System.out.println("Enter first number\n");
+			x = sc.nextInt();
+			
+			System.out.println("Enter second number\n");
+			y = sc.nextInt();
+		
+			int add = x+y;
+			int sub = x-y;
+			int mult = x*y;
+			int div = x/y;
+						
+			System.out.println("Addition is : "+add);
+			System.out.println("Substraction is : "+sub);
+			System.out.println("Multiplication is : "+mult);
+			System.out.println("Division is : "+div);
+
+
+				
+		
+
+	}
+
+
+
+
+
+
+
+
+}
