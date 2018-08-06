8ca753ed9917e76a87ea180c244a2daa0cf4f0ef
Shrimant More <smshrimant@gmail.com>
2018-08-07 01:24:35 +0530

    New file added

---
+++ student1.java
@@ -0,0 +1,18 @@
+//Static program to display name and ID
+
+class student1
+{
+	int id=2;
+	String name = "Shrimant12";
+
+
+	public static void main(String args[])
+	{
+	
+		student1 s1 = new student1();
+		System.out.println(s1.id);
+		System.out.println(s1.name);
+
+
+}
+}
